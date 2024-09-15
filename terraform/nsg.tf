e22640a1291d1395e4039dc39e45e1182f9795d1
johnbedeir <john.bedeir@gmail.com>
2024-09-15 18:53:03 +0200

    Fix #2

---
+++ terraform/nsg.tf
@@ -0,0 +1,44 @@
+# Create Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.name_prefix}-${var.environment}-nsg"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_network_security_rule" "allow_sql_server_traffic" {
+  name                        = "AllowSQLServerTraffic"
+  priority                    = 100   # Ensure this priority is different from other rules
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["1433"]
+  source_address_prefix       = "10.0.2.0/24"
+  destination_address_prefix  = "*"
+  resource_group_name = azurerm_resource_group.aks.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "allow_sql_outbound" {
+  name                        = "allow_sql_outbound"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "1433"
+  source_address_prefix       = "*"  # Your AKS subnet CIDR block
+  destination_address_prefix  = "10.0.2.0/24"
+  resource_group_name         = azurerm_resource_group.aks.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# Associate NSG with the subnet where the private endpoint is deployed
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.private_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
