b47e4607ba2007f8be98bb0fb88b7da2d3165db5
johnbedeir <john.bedeir@gmail.com>
2024-09-22 18:24:40 +0200

    Fix #11 argo prometheus alertmanager access

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -11,12 +11,12 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_network_security_rule" "allow_http_inbound" {
   name                        = "AllowHTTPInbound"
-  priority                    = 200
+  priority                    = 220
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "80"
+  destination_port_ranges     = ["80", "443", "9090", "9093"]
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.aks.name
