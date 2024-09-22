9a631959494aefd1f75c8dca7591fa6597d70ca6
johnbedeir <john.bedeir@gmail.com>
2024-09-22 18:13:26 +0200

    Fix #6 create table manually

--- todo-app/init-db.sql
+++ todo-app/init-db.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS tasks (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  description TEXT,
-  is_complete BOOLEAN DEFAULT false
-);
+BEGIN
+    CREATE TABLE tasks (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        title NVARCHAR(255) NOT NULL,
+        description NVARCHAR(MAX),
+        is_complete BIT DEFAULT 0
+    )
+END
