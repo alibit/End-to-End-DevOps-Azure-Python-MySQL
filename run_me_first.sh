21cf985dded4d3929e466e7693520b1232a02614
johnydev-1 <gitlab.imply909@simplelogin.com>
2025-01-06 00:58:25 -0500

    [skip ci] update run me first script

--- run_me_first.sh
+++ run_me_first.sh
@@ -3,12 +3,17 @@
 # Variables
 subscription_id=$(cat subscription.txt)
 service_perincipal_name="pyapp"
+federated_credential_name="GitHubActions"
+repo_name="johnbedeir/End-to-End-DevOps-Azure-Python-MySQL"
+branch_name="main"
+issuer="https://token.actions.githubusercontent.com"
+audience="api://AzureADTokenExchange"
+description="GitHub Actions OIDC Federated Credential"
 # End of Variables
 
 # Check if the service principal already exists
 sp_exists=$(az ad sp list --display-name $service_perincipal_name --query "[?appDisplayName=='$service_perincipal_name'].appId" -o tsv)
 
-# If the service principal doesn't exist, create it
 if [ -z "$sp_exists" ]; then
     echo "Service principal does not exist. Creating a new one..."
     service_perincipal=$(az ad sp create-for-rbac --name "$service_perincipal_name" --role Contributor --scopes /subscriptions/$subscription_id)
@@ -23,4 +28,18 @@ if [ -z "$sp_exists" ]; then
     echo "Tenant ID: $TENANT_ID"
 else
     echo "Service principal already exists with App ID: $sp_exists"
+    CLIENT_ID=$sp_exists
 fi
+
+# Add Federated Credential
+echo "Adding federated credentials to the service principal..."
+az ad app federated-credential create --id $CLIENT_ID \
+    --parameters "{
+        \"name\": \"$federated_credential_name\",
+        \"issuer\": \"$issuer\",
+        \"subject\": \"repo:$repo_name:ref:refs/heads/$branch_name\",
+        \"audiences\": [\"$audience\"],
+        \"description\": \"$description\"
+    }"
+
+echo "Federated credentials added successfully."
